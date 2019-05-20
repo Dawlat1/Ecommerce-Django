c0a0025462099b5d81c3e0afb8a9610569fcadef
Dawlat1 <dawlat.moustafa2@gmail.com>
2019-05-20 21:03:23 +0100

    products app update

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView 
+from django.shortcuts import render, get_object_or_404
+
+from .models import Product
 
 # Create your views here.
+
+class ProductListView(ListView):
+	queryset = Product.objects.all()
+	template_name = "products/list.html"
+
+
+def product_list_view(request):
+	queryset = Product.objects.all()
+	context = {
+		'object_list': queryset
+	}
+	return render(request, "products/list.html", context)
+
+
+class ProductDetailView(ListView):
+	queryset = Product.objects.all()
+	template_name = "products/detail.html"
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+		return context
+
+
+def product_detail_view(request, pk=None, *args, **kwargs):
+	#instance = Product.objects.get(pk=pk) #id
+	instance = get_object_or_404(Product, pk=pk)
+	context = {
+		'object': instance
+	}
+	return render(request, "products/detail.html", context)
